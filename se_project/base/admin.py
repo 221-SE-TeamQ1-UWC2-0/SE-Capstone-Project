d2c8c4d135c33ac77ed9d2aa95341c8fa773c96c
Quang Nguyen <78084202+quang-ngh@users.noreply.github.com>
2022-10-14 12:59:05 +0700

    rebase the data model for backoff

--- se_project/base/admin.py
+++ se_project/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+admin.site.register(models.MCP)
+admin.site.register(models.Vehicle)
+admin.site.register(models.Route)
 
 # Register your models here.
--- se_project/base/models.py
+++ se_project/base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+"""
+Create a pattern for ID of MCP, Vehicle, Route, Person
+"""
+class MCP(models.Model):
+
+    id = models.CharField(max_length=100, primary_key = True)
+    location = models.JSONField()
+    capacity = models.FloatField()
+
+class Vehicle(models.Model):
+
+    id = models.CharField(max_length=100, primary_key = True)
+    fuel_capacity = models.FloatField()
+    capacity = models.FloatField()
+
+class Route(models.Model):
+
+    id = models.CharField(max_length=100, primary_key= True)
+    seq_mcps_id = models.JSONField()
+    vehicle_id = models.CharField(max_length=100)
