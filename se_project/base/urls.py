9b1045a1bb6b19313428560a38af332354c58c70
chanhtran2410 <tranletrungchanh12345@gmail.com>
2022-10-26 11:11:29 +0700

    update sidebar

--- se_project/base/urls.py
+++ se_project/base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('map/', views.map, name = 'map'),
     path('index/', views.index, name = 'index')
+
 ]
--- se_project/base/views.py
+++ se_project/base/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def map(request):
+    return render(request, 'map.html')
+
 def index(request):
     return render(request, 'index.html')
