ed461053bc29710e0862d2a3ffbc29f5471fb7e0
chanhtran2410 <tranletrungchanh12345@gmail.com>
2022-11-15 15:20:50 +0700

    update janitor info

--- se-react/src/component/staff-info/test.js
+++ se-react/src/component/staff-info/test.js
@@ -1,6 +1,7 @@
 import "./janitor.css";
-import React, { useState, Component, useEffect } from "react";
-import DataList from "./DataList";
+import React, { useState, Component } from "react";
+import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
 
 
 import {
@@ -46,9 +47,59 @@ const Logo = () => {
     );
 };
 
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+function Items({ currentItems }) {
+    return (
+      <>
+        {currentItems &&
+          currentItems.map((item) => (
+                <div className="listitem">
+                    <h3>Item #{item}</h3>
+                </div>
+          ))}
+      </>
+    );
+  }
+  
+  function PaginatedItems({ itemsPerPage }) {
+    // Here we use item offsets; we could also use page offsets
+    // following the API or data you're working with.
+    const [itemOffset, setItemOffset] = useState(0);
+  
+    // Simulate fetching items from another resources.
+    // (This could be items from props; or items loaded in a local state
+    // from an API endpoint with useEffect and useState)
+    const endOffset = itemOffset + itemsPerPage;
+    const currentItems = items.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(items.length / itemsPerPage);
+  
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % items.length;
+      setItemOffset(newOffset);
+    };
+  
+    return (
+      <>
+        <Items currentItems={currentItems} />
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=" >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< "
+          renderOnZeroPageCount={null}
+        />
+      </>
+    );
+  }
+  
+
+
 
 function Janitor(){
-    
     /*Return function*/    
     return (
         <div className="container">
@@ -130,7 +181,9 @@ function Janitor(){
                     </div>
                 </div>
                 <div className="main">
-                    <DataList/>
+                    <div id="list">
+                        <PaginatedItems itemsPerPage={8} />,
+                    </div>
                 </div>
 
             </div>
