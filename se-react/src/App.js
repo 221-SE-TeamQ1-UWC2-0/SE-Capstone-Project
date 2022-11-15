ed9156c68bd4674cc6c2f0702c5c1dceca788709
Huy Duong Ngoc Quang <huy.duonghe1nz@hcmut.edu.vn>
2022-11-15 13:17:46 +0700

    Private Route and AuthProvider update

--- se-react/src/App.js
+++ se-react/src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
-import Dashboard from './component/Dashboard/dashboard'
-import Janitor from './component/staff-info/janitor-info'
-import LoginView from './component/authenticate/login'
-import RegisterView from './component/authenticate/register'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import Dashboard from './component/Dashboard/dashboard';
+import Janitor from './component/staff-info/janitor-info';
+import LoginView from './component/authenticate/login';
+import RegisterView from './component/authenticate/register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './utils/privateRoute';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
-
-if(false)
-  return <LoginView/>
   return (
-    <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/register" element={<RegisterView />} />
-            <Route path="/login" element={<LoginView />} />
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/janitor-info" element={<Janitor />} />
-          </Routes>
-        </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/register' element={<RegisterView />} />
+          <Route path='/login' element={<LoginView />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/janitor-info' element={<Janitor />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
