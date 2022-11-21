7a43b7e43c2f319f116dd858c61c7eb66380b840
Huy Duong Ngoc Quang <huy.duonghe1nz@hcmut.edu.vn>
2022-11-21 14:25:40 +0700

    Task API update

--- se_project/api/urls.py
+++ se_project/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserTokenObtainPairView, UserViewSet
+from .views import UserTokenObtainPairView, UserViewSet, TaskViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('user', UserViewSet)
+router.register('task', TaskViewSet)
 
 urlpatterns = [
     path('token/', UserTokenObtainPairView.as_view(), name='token_obtain'),
