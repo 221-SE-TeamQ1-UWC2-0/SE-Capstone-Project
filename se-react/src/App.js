c1affc4cc7fd15afa5c76a00dc6567776d58b08e
hoangg-ng <ngmhoang2306@gmail.com>
2022-12-06 11:09:13 +0700

    add task

--- se-react/src/App.js
+++ se-react/src/App.js
@@ -9,8 +9,7 @@ import Mapbox from './component/map/map';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PrivateRoute from './utils/privateRoute';
 import { AuthProvider } from './context/AuthContext';
-import Task from './component/app/task';
-import Mhome from './component/app/mhome';
+import Task from './component/staff-info/addtask';
 
 function App() {
   // if (true) return <Task />
@@ -25,7 +24,6 @@ function App() {
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/janitor-info' element={<Janitor />} />
             <Route path='/task' element={<Task />} />
-            <Route path='/mhome' element={<Mhome />} />
             <Route path='/collector-info' element={<Collector />} />
             <Route path='/edit' element={<Edit />} />
             <Route path='/map' element={<Mapbox />} />
